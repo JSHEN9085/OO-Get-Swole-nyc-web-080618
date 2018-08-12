4d6a1e0e54b57d44fd83a892be0d41e949df39fc
github username <github email address>
2018-08-12 18:37:04 -0400

    Done.

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -6,11 +6,34 @@ class Gym
   def initialize(name)
     @name = name
     ALL << self
-
   end
 
   def self.all
     ALL
+  end
+
+  def memberships
+    Membership.all.select do |membership|
+      membership.gym == self
+    end
+  end
+
+  def lifters
+    memberships.map do |membership|
+      membership.lifter
+    end
+  end
+
+  def lifter_names
+    lifters.collect do |lifter|
+      lifter.name
+  end
+
+  def lift_total_for_gym
+    total = lifters.map do |lifter|
+      lifter.lift_total
+    end
+    total.sum 
   end
 
 end
--- app/models/lifter.rb
+++ app/models/lifter.rb
@@ -1,16 +1,50 @@
 
 class Lifter
 
-
+  @@all = []
 
   attr_reader :name, :lift_total
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
-
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select do |membership|
+      membership.lifter == self
+    end
+  end
+
+  def gyms
+    memberships.map do |membership|
+      membership.gym
+    end
+  end
+
+  def self.average_lift
+    sum = 0
+    Lifter.all.each do |lifter|
+      sum += lifter.lift_total
+    end
+    sum.to_f / Lifter.all.length
+  end
+
+  def sign_up(cost, gym)
+    Membership.new(cost, self, gym)
+  end
+
+  def total_cost
+    cost = memberships.map do |membership|
+      membership.cost
+    end
+    cost.sum
+  end
 
 
 end
